63b2d13e4ef76872fdd65221b2ce1cfbebd0803d
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-10-06 18:59:57 +0530

    commied

--- src/components/circle.js
+++ src/components/circle.js
@@ -5,3 +5,22 @@
 
 // export
 
+import React from "react";
+import Shape from "./Shape";
+
+class Circle extends Shape {
+  drawShape() {
+    console.log("Draw Circle");
+  }
+  calculateArea() {
+    console.log("Circle Area");
+  }
+  color() {
+    console.log("Circle Color");
+  }
+  render() {
+    return <h1>Circle</h1>;
+  }
+}
+
+export default Cirlce;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
 
 // export class
+import React from "react";
+
+class Shape extends React.Component {
+  drawShape() {
+    console.log("Draw!!!");
+  }
+  calculateArea() {
+    console.log("Area!!!");
+  }
+  color() {
+    console.log("Color!!!");
+  }
+  render() {
+    return <h1>Shape</h1>;
+  }
+}
+
+export default Shape;
